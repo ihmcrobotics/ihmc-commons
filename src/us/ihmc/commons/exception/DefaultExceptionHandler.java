b4113c43ea537aaea7a30aff2de8798af2538240
Duncan Calvert <dcalvert@ihmc.us>
2017-03-09 17:10:07 -0600

    Add documentation.

--- src/us/ihmc/commons/exception/DefaultExceptionHandler.java
+++ src/us/ihmc/commons/exception/DefaultExceptionHandler.java
@@ -2,10 +2,29 @@ package us.ihmc.commons.exception;
 
 import us.ihmc.commons.PrintTools;
 
+/**
+ * Create awareness, expliciness, and ease of handling exceptions in default or common ways.
+ */
 public enum DefaultExceptionHandler
 {
-   PROCEED_SILENTLY, KILL_PROCESS, PRINT_STACKTRACE, PRINT_MESSAGE;
-   
+   /** Does nothing. */
+   PROCEED_SILENTLY,
+
+   /** Runs System.exit(1), killing the process and indicating failure. */
+   KILL_PROCESS,
+
+   /** Prints the stack trace. */
+   PRINT_STACKTRACE,
+
+   /** Prints the throwable's message in a friendly way using {@link PrintTools} */
+   PRINT_MESSAGE;
+
+   /**
+    * Handles the throwable in one of the default ways.
+    * 
+    * @param throwable to be handled
+    * @return Null casted to Object for convenience.
+    */
    public Object handleException(Throwable throwable)
    {
       switch (this)
@@ -23,7 +42,7 @@ public enum DefaultExceptionHandler
          PrintTools.error(this, throwable.getMessage());
          break;
       }
-      
+
       return null;
    }
 }
