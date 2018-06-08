129e1082795d9d14cbf43975fc750a4c77ec710b
Stephen McCrory <smccrory@ihmc.us>
2018-06-08 14:17:49 -0500

    Adding documentation to SupplierBuilder

--- src/main/java/us/ihmc/robotics/lists/SupplierBuilder.java
+++ src/main/java/us/ihmc/robotics/lists/SupplierBuilder.java
@@ -6,6 +6,22 @@ import java.util.function.Supplier;
 
 public class SupplierBuilder
 {
+   /**
+    * Creates a supplier such that calling {@link Supplier#get} returns
+    * an instance of the given class type when calling its empty constructor
+    *
+    * <p> For example, the following is equivalent to calling {@code Thread thread = new Thread();}
+    *
+    * <p> {@code Supplier<Thread> supplier = SupplierBuilder.createFromEmptyConstructor(Thread.class);}
+    * <p> {@code Thread thread = supplier.get();}
+    *
+    * @param clazz class of supplied object
+    * @param <T> type of supplied object
+    * @return supplier
+    *
+    * @exception RuntimeException if the class does not have an accessible empty constructor, or the constructing
+    * class has one of the issues described in {@link Constructor#newInstance}
+    */
    public static <T> Supplier<T> createFromEmptyConstructor(Class<T> clazz)
    {
       final Constructor<T> emptyConstructor;
@@ -17,7 +33,7 @@ public class SupplierBuilder
       {
          throw new RuntimeException("Could not find a visible empty constructor in the class: " + clazz.getSimpleName());
       }
-
+      
       return () ->
       {
          try
