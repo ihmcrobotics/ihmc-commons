1d3da735e14e5b5aeda1a0a1923545804624f201
Duncan Calvert <dcalvert@ihmc.us>
2018-10-18 13:34:52 -0500

    Switch to use ihmc-ci for allocation testing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
    id("us.ihmc.ihmc-build") version "0.15.1"
    id("us.ihmc.log-tools") version "0.2.2"
+   id("us.ihmc.ihmc-ci") version "1.0.2"
 }
 
 ihmc {
@@ -30,43 +31,4 @@ ihmc.sourceSetProject("testing").dependencies {
 ihmc.sourceSetProject("test").dependencies {
    compile(ihmc.sourceSetProject("main"))
    compile(ihmc.sourceSetProject("testing"))
-   implementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-   runtimeOnly("org.junit.vintage:junit-vintage-engine:5.3.1")
-   runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-}
-
-val allocationTests = (property("allocationTests") as String) == "true"
-
-ihmc.sourceSetProject("test").tasks.withType<Test> {
-   useJUnitPlatform {
-      if (allocationTests)
-      {
-         includeTags("allocation")
-      }
-      else
-      {
-         excludeTags("allocation")
-      }
-   }
-
-   setForkEvery(1)
-   maxParallelForks = 2
-   systemProperties["junit.jupiter.execution.parallel.enabled"] = "true"
-   systemProperties["junit.jupiter.execution.parallel.config.strategy"] = "dynamic"
-   systemProperties["junit.jupiter.execution.parallel.config.fixed.parallelism"] = "4"
-
-   if (allocationTests)
-   {
-      ihmc.sourceSetProject("test").configurations.compile.files.forEach {
-         if (it.name.contains("java-allocation-instrumenter"))
-         {
-            val jvmArg = "-javaagent:" + it.getAbsolutePath()
-            println("[ihmc-commons] Passing JVM arg: " + jvmArg)
-            val tmpArgs = allJvmArgs
-            tmpArgs.add(jvmArg)
-            allJvmArgs = tmpArgs
-
-         }
-      }
-   }
 }
