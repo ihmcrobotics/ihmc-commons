1cbe7e76dfebc10d35497086dc05b3ce3186df15
Robert <rgriffin@ihmc.us>
2019-04-28 16:11:40 -0500

    :bookmark: 0.26.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.26.2"
+   version = "0.26.3"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-commons"
    openSource = true
    maintainer = "Duncan Calvert"
