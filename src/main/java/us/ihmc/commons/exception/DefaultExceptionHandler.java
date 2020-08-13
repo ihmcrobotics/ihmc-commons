c90983309ba21aa6440856d3705fb1881236c330
Duncan Calvert <dcalvert@ihmc.us>
2020-08-12 20:48:53 -0500

    Add final to DefaultExceptionHandlers and add a new common usage.

--- src/main/java/us/ihmc/commons/exception/DefaultExceptionHandler.java
+++ src/main/java/us/ihmc/commons/exception/DefaultExceptionHandler.java
@@ -8,20 +8,27 @@ import us.ihmc.log.LogTools;
 public class DefaultExceptionHandler
 {
    /** Does nothing. */
-   public static ExceptionHandler PROCEED_SILENTLY = e -> { };
-
-   /** Runs System.exit(1), killing the process and indicating failure. */
-   public static ExceptionHandler KILL_PROCESS = e -> {
-      LogTools.error(e.getMessage());
-      System.exit(1);
-   };
+   public static final ExceptionHandler PROCEED_SILENTLY = e -> { };
 
    /** Prints the stack trace. */
-   public static ExceptionHandler PRINT_STACKTRACE = Throwable::printStackTrace;
+   public static final ExceptionHandler PRINT_STACKTRACE = Throwable::printStackTrace;
 
    /** Throws a {@link RuntimeException} */
-   public static ExceptionHandler RUNTIME_EXCEPTION = e -> { throw new RuntimeException(e); };
+   public static final ExceptionHandler RUNTIME_EXCEPTION = e -> { throw new RuntimeException(e); };
 
    /** Prints the throwable's message in a friendly way using {@link LogTools} */
-   public static ExceptionHandler PRINT_MESSAGE = e -> LogTools.error(e.getMessage());
+   public static final ExceptionHandler PRINT_MESSAGE = e -> LogTools.error(e.getMessage());
+
+   public static final ExceptionHandler MESSAGE_AND_STACKTRACE = e ->
+   {
+      LogTools.error(e.getMessage());
+      e.printStackTrace();
+   };
+
+   /** Runs System.exit(1), killing the process and indicating failure. */
+   public static final ExceptionHandler KILL_PROCESS = e -> {
+      LogTools.error(e.getMessage());
+      e.printStackTrace();
+      System.exit(1);
+   };
 }
