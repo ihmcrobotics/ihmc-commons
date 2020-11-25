a5491df1bd3338f184dafe0563aa42c735895b29
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 22:08:12 -0600

    Upgrade dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
    id("us.ihmc.ihmc-build") version "0.22.0"
    id("us.ihmc.log-tools-plugin") version "0.5.0"
    id("us.ihmc.ihmc-ci") version "7.0"
-   id("us.ihmc.ihmc-cd") version "1.14"
+   id("us.ihmc.ihmc-cd") version "1.16"
 }
 
 ihmc {
    group = "us.ihmc"
-   version = "0.30.3"
+   version = "0.30.4"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-commons"
    openSource = true
    maintainer = "Duncan Calvert"
@@ -21,16 +21,16 @@ categories.configure("allocation") {
 }
 
 mainDependencies {
-   api("org.apache.commons:commons-lang3:3.9")
-   api("commons-io:commons-io:2.6")
+   api("org.apache.commons:commons-lang3:3.11")
+   api("commons-io:commons-io:2.8.0")
    api("us.ihmc:log-tools:0.5.0")
 }
 
 testingDependencies {
    api(ihmc.sourceSetProject("main"))
    api(junit.jupiterApi())
-   api("org.pitest:pitest-command-line:1.4.10")
-   api("org.pitest:pitest-junit5-plugin:0.9")
+   api("org.pitest:pitest-command-line:1.5.2")
+   api("org.pitest:pitest-junit5-plugin:0.12")
    api(allocation.instrumenter()) // should be runtimeOnly?
 }
 
