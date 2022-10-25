c2e6e7da414fc1999c4320388bae05374dd44726
Duncan Calvert <dcalvert@ihmc.us>
2022-10-24 23:32:26 -0500

    Add peek methods to Notification in order to make it useful in more situations.

--- src/main/java/us/ihmc/commons/thread/Notification.java
+++ src/main/java/us/ihmc/commons/thread/Notification.java
@@ -53,6 +53,29 @@ public class Notification
    private boolean previousValue = false;
 
    /**
+    * Peeks at the value of the notification without clearing it.
+    *
+    * @return if the notification has been set
+    */
+   public boolean peek()
+   {
+      return notification;
+   }
+
+   /**
+    * If notification not already set, block and wait to be notified.
+    * Does not clear this notification.
+    */
+   public synchronized void blockingPeek()
+   {
+      if (!peek())
+      {
+         ExceptionTools.handle(() -> this.wait(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
+         peek();
+      }
+   }
+
+   /**
     * Polls and clears the notification.
     *
     * @return if notification was set
@@ -75,14 +98,12 @@ public class Notification
 
    /**
     * If notification not already set, block and wait to be notified.
+    * Clears this notification.
     */
    public synchronized void blockingPoll()
    {
-      if (!poll())
-      {
-         ExceptionTools.handle(() -> this.wait(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
-         poll();
-      }
+      blockingPeek();
+      poll();
    }
 
    /**
