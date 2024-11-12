1b094051f14e75a0620988c2a22bd1498992da53
Tomasz Bialek <103042423+TomaszTB@users.noreply.github.com>
2024-11-12 16:51:49 -0600

    Implement RepeatingTaskThread (#36)
    Co-authored-by: Duncan Calvert <dcalvert@ihmc.org>
    Co-authored-by: ds58

---
+++ src/main/java/us/ihmc/commons/thread/RepeatingTaskThread.java
@@ -0,0 +1,448 @@
+package us.ihmc.commons.thread;
+
+import us.ihmc.commons.Conversions;
+import us.ihmc.commons.RunnableThatThrows;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.exception.ExceptionHandler;
+import us.ihmc.commons.exception.ExceptionTools;
+
+/**
+ * A thread that repeats the execution of a single task. It can do this {@code n} times, continuously, or at a limited rate.
+ * <p>
+ * Unlike {@link Thread}, this class ensures that exceptions are caught and handled.
+ * <p>
+ *  The task to execute may be specified in two ways:
+ *  <ol>
+ *     <li> Providing a {@link RunnableThatThrows} during construction.
+ *     <li> {@code @Override}ing the {@link #runTask()} method.
+ *  </ol>
+ * <p>
+ * Upon construction, this thread will have zero scheduled task executions and the thread will not be started.
+ * Execution can be started in two ways:
+ *  <ol>
+ *     <li> Call {@link #startRepeating()}
+ *     <li> Call {@link #start()}, and {@link #setScheduled(long n)} or {@link #addScheduled(long n)} in any order.
+ *  </ol>
+ * <p>
+ * The execution frequency may be limited with {@link #setFrequencyLimit(double)} and removed using {@link #removeFrequencyLimit()}.
+ * The limit can be modified or removed at any time.
+ * Note that this is only an upper limit. If the task overruns the corresponding period, the task will simply execute again
+ * once its finished, resulting in a potentially jittery, slower frequency.
+ * <p>
+ * To support clear and concise initialization, {@link #setFrequencyLimit} returns {@code this}.
+ * <p>
+ * To allow the currently executing task to finish, if one is executing, and allow the thread to die,
+ * use {@link #kill()} or {@link #blockingKill()}.
+ */
+public class RepeatingTaskThread extends Thread
+{
+   public static final int REPEAT_INDEFINITELY = -1;
+   public static final double UNLIMITED_FREQUENCY = -1.0;
+
+   private final RunnableThatThrows task;
+   private final ExceptionHandler exceptionHandler;
+
+   /**
+    * Becomes {@code true} when the thread is started, and {@code false} when the thread is killed.
+    * Once {@code false}, the task loop will allow the currently executing task (if any) to complete,
+    * and the task loop is exited, allowing the thread to die.
+    */
+   private volatile boolean running = false;
+   
+   /** Execution state of this thread */
+   private final ExecutionState executionState = new ExecutionState();
+
+   /** Throttler used to limit the execution frequency. */
+   private final Throttler throttler = new Throttler();
+
+   /** The period of the set frequency limit. A negative value indicates no limit. */
+   private double periodLowerLimit = UNLIMITED_FREQUENCY;
+
+   public RepeatingTaskThread(String name)
+   {
+      this(name, DefaultExceptionHandler.MESSAGE_AND_STACKTRACE);
+   }
+
+   public RepeatingTaskThread(String name, ExceptionHandler exceptionHandler)
+   {
+      this(name, null, exceptionHandler);
+   }
+
+   public RepeatingTaskThread(String name, RunnableThatThrows task)
+   {
+      this(name, task, DefaultExceptionHandler.MESSAGE_AND_STACKTRACE);
+   }
+
+   public RepeatingTaskThread(String name, RunnableThatThrows task, ExceptionHandler exceptionHandler)
+   {
+      super(name);
+      this.task = task;
+      this.exceptionHandler = exceptionHandler;
+   }
+
+   /**
+    * Limits the frequency of task execution.
+    * <p>
+    * This is only an upper limit. If the task overruns the corresponding period, the task will simply execute again
+    * once its finished, resulting in a potentially jittery, slower frequency.
+    * <p>
+    * To remove the limit, use {@link #removeFrequencyLimit()} or set to {@link #UNLIMITED_FREQUENCY}.
+    *
+    * @param frequencyLimit The frequency limit or {@link #UNLIMITED_FREQUENCY}.
+    * @return {@code this}, such that it can be used like a
+    *       <a href="https://en.wikipedia.org/wiki/Fluent_interface">fluent interface</a>.
+    */
+   public RepeatingTaskThread setFrequencyLimit(double frequencyLimit)
+   {
+      periodLowerLimit = Conversions.hertzToSeconds(frequencyLimit);
+      return this;
+   }
+
+   /**
+    * Removes the frequency limit.
+    * <p>
+    * Equivalent to {@code setFrequencyLimit(UNLIMITED_FREQUENCY)}.
+    */
+   public void removeFrequencyLimit()
+   {
+      setFrequencyLimit(UNLIMITED_FREQUENCY);
+   }
+
+   @Override
+   public void start()
+   {
+      running = true;
+      super.start();
+   }
+
+   /**
+    * Ensures the thread is started and schedules indefinitely repeating execution.
+    */
+   public void startRepeating()
+   {
+      if (!running)
+         start();
+
+      setRepeating(true);
+   }
+
+   /**
+    * Clears the execution schedule. If the task is currently executing,
+    * it is allowed to complete.
+    */
+   public void stopRepeating()
+   {
+      setRepeating(false);
+   }
+
+   /**
+    * @param repeating If {@code true}, schedules indefinitely repeating execution.
+    *                  If {@code false}, clears the execution schedule.
+    */
+   public void setRepeating(boolean repeating)
+   {
+      if (repeating)
+         setScheduled(REPEAT_INDEFINITELY);
+      else
+         setScheduled(0);
+   }
+
+   /**
+    * Sets the execution schedule to {@code n} executions.
+    * <p>
+    * If the task was not executing and {@code n > 0}, it will immediately begin executing.
+    *
+    * @param n The updated number of scheduled executions.
+    */
+   public void setScheduled(long n)
+   {
+      executionState.setScheduled(n);
+   }
+
+   /**
+    * Schedules {@code n} more executions.
+    * <p>
+    * If the task was not executing and {@code n > 0}, it will immediately begin executing.
+    * <p>
+    * You may also subtract from the number of scheduled executions by passing in a negative number.
+    * This method cannot cause the scheduled execution count to go below 0.
+    * <p>
+    * This method does not do anything if indefinitely repeating execution is scheduled.
+    *
+    * @param n If {@code n > 0}, the number of executions to schedule.
+    *          If {@code n < 0}, the number of executions to unschedule.
+    */
+   public void addScheduled(long n)
+   {
+      executionState.addScheduled(n);
+   }
+
+   /**
+    * @return The number of scheduled executions.
+    */
+   public long getScheduled()
+   {
+      return executionState.getScheduled();
+   }
+
+   /**
+    * @return Whether a task is currently executing.
+    */
+   public boolean isExecuting()
+   {
+      return executionState.isExecuting();
+   }
+
+   /**
+    * @return {@code true} if the thread has been started via {@link #start}
+    *          and not yet killed via {@link #kill} or {@link #blockingKill}.
+    */
+   public boolean isRunning()
+   {
+      return running;
+   }
+
+   /**
+    * @return The total number of task executions completed by this thread.
+    */
+   public long getCompleted()
+   {
+      return executionState.getCompleted();
+   }
+
+   /**
+    * Block until the next start of the task.
+    * @return {@code true} if interrupted.
+    */
+   public boolean blockUntilNextTaskExecution()
+   {
+      synchronized (executionState)
+      {
+         do
+         {
+            if (executionState.waitForChange())
+               return true;
+
+         } while (!executionState.isExecuting());
+      }
+
+      return false;
+   }
+
+   /**
+    * Block until the next completion of the task.
+    * @return {@code true} if interrupted.
+    */
+   public boolean blockUntilNextTaskCompletion()
+   {
+      synchronized (executionState)
+      {
+         long completedBefore = executionState.getCompleted();
+         while (completedBefore == executionState.getCompleted())
+         {
+            if (executionState.waitForChange())
+               return true;
+         }
+      }
+
+      return false;
+   }
+
+   /**
+    * Block until there are no scheduled task executions, which may be immediately.
+    * @return {@code true} if interrupted.
+    */
+   public boolean blockUntilNoScheduledTasks()
+   {
+      synchronized (executionState)
+      {
+         while (executionState.getScheduled() != 0 || executionState.isExecuting())
+         {
+            if (executionState.waitForChange())
+               return true;
+         }
+      }
+
+      return false;
+   }
+
+   /**
+    * Allows the thread to die.
+    * Any currently executing task will first be allowed to finish.
+    * This class cannot be reused after this point.
+    */
+   public void kill()
+   {
+      running = false;
+      executionState.setScheduled(0);
+   }
+
+   /**
+    * Allows the thread to die and blocks until it does.
+    * Any currently executing task will first be allowed to finish.
+    * This class cannot be reused after this point.
+    * <p>
+    * Same as calling {@link #kill()} then {@link #join()}.
+    * @return {@code true} if interrupted.
+    */
+   public boolean blockingKill()
+   {
+      kill();
+      try
+      {
+         join();
+      }
+      catch (InterruptedException e)
+      {
+         return true;
+      }
+
+      return false;
+   }
+
+   /**
+    * The task that is executed repeatedly.
+    * <p>
+    * You may {@code @Override} this method with the code to execute.
+    * Otherwise, this method will execute the passed in {@link RunnableThatThrows}.
+    *
+    * @throws Throwable Any throwable that the executed code may throw.
+    *       Will be handled by the {@link ExceptionHandler}
+    *       (default: {@link DefaultExceptionHandler#MESSAGE_AND_STACKTRACE}).
+    */
+   protected void runTask() throws Throwable
+   {
+      if (task != null)
+         task.run();
+   }
+
+   /**
+    * The {@link Thread#run()} method, overridden to run a loop.
+    * To extend this class {@link Thread} style, override {@link #runTask()} instead.
+    * <p>
+    * DO NOT CALL THIS METHOD.
+    * The existence of this method is a necessary evil for this class to extend Thread.
+    */
+   @Override
+   public final void run()
+   {
+      while (running)
+      {
+         synchronized (executionState)
+         {
+            if (executionState.getScheduled() == 0L)
+            {
+               executionState.waitForChange();
+               continue;
+            }
+         }
+
+         // If a period/frequency limit was set, wait until the next period has started.
+         if (periodLowerLimit > 0.0)
+         {
+            // Uses ThreadTools.parkAtLeast, which will not throw InterruptedException, but
+            // will return early when this thread is interrupted.
+            throttler.waitAndRun(periodLowerLimit);
+         }
+
+         // 1. Clears the interrupt status, so we don't run the task with it.
+         // 2. Allows the user to use interrupt to get the throttler to stop waiting.
+         // 3. We won't execute the task if there's nothing scheduled or kill has been called.
+         // 4. Otherwise, we'll go ahead and execute the next task.
+         if (interrupted() && (!running || executionState.getScheduled() == 0L))
+            continue;
+
+         // Run the runTask method, and handle any exception it may throw.
+         executionState.beforeTaskExecution();
+         ExceptionTools.handle(this::runTask, exceptionHandler);
+         executionState.afterTaskExecution();
+      }
+   }
+
+   /** The execution state of the RepeatingTaskThread. */
+   private static class ExecutionState
+   {
+      /**
+       * How many more times to execute the task.
+       *
+       * <ul>
+       *    <li> n > 0 = Task will be executed n more times.
+       *    <li> 0 = Task will not be executed again.
+       *    <li> -1 = Task will be executed repeatedly and indefinitely.
+       */
+      private volatile long scheduledRepetitions = 0L;
+
+      /** Whether the task is currently executing. */
+      private volatile boolean executing = false;
+
+      /** The total number of times the task has completed execution during the lifetime of this thread. */
+      private volatile long completedRepetitions = 0L;
+
+      private synchronized void beforeTaskExecution()
+      {
+         if (scheduledRepetitions > 0)
+            --scheduledRepetitions;
+         executing = true;
+         notifyAll();
+      }
+
+      private synchronized void afterTaskExecution()
+      {
+         executing = false;
+         ++completedRepetitions;
+         notifyAll();
+      }
+
+      private synchronized void setScheduled(long repetitions)
+      {
+         scheduledRepetitions = repetitions;
+         notifyAll();
+      }
+
+      private synchronized void addScheduled(long repetitions)
+      {
+         // If repeating indefinitely, do nothing
+         if (scheduledRepetitions < 0L)
+            return;
+
+         // Add to the scheduled repetition counter, ensuring it doesn't become negative
+         scheduledRepetitions = Math.max(scheduledRepetitions + repetitions, 0L);
+
+         notifyAll();
+      }
+
+      /**
+       * Wait until a change occurs to the thread's execution state.
+       *
+       * @return {@code true} if interrupted.
+       */
+      private synchronized boolean waitForChange()
+      {
+         try
+         {
+            wait();
+         }
+         catch (InterruptedException e)
+         {
+            return true;
+         }
+
+         return false;
+      }
+
+      private long getScheduled()
+      {
+         return scheduledRepetitions;
+      }
+
+      private boolean isExecuting()
+      {
+         return executing;
+      }
+
+      private long getCompleted()
+      {
+         return completedRepetitions;
+      }
+   }
+}
---
+++ src/test/java/us/ihmc/commons/thread/RepeatingTaskThreadTest.java
@@ -0,0 +1,423 @@
+package us.ihmc.commons.thread;
+
+import org.junit.jupiter.api.Test;
+import us.ihmc.commons.Conversions;
+import us.ihmc.commons.RunnableThatThrows;
+import us.ihmc.commons.time.FrequencyCalculator;
+import us.ihmc.log.LogTools;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RepeatingTaskThreadTest
+{
+   private static final String NAME = "TestRepeatingTaskThread";
+
+   @Test
+   public void testStartKill()
+   {
+      AtomicBoolean taskRan = new AtomicBoolean(false);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, () -> taskRan.set(true));
+
+      // Start the thread, but don't run anything
+      thread.start();
+      assertState(thread, true, true, 0, false, 0);
+
+      // Kill the thread, wait for it to die
+      thread.kill();
+      assertState(thread, false, true, 0, false, 0);
+      assertDoesNotThrow(() -> thread.join(1000));
+      assertState(thread, false, false, 0, false, 0);
+
+      // Ensure the task never ran
+      assertFalse(taskRan.get());
+   }
+
+   @Test
+   public void testStartRepeatKill()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, () ->
+      {
+         LogTools.info("Repetition {}", repetitions.getAndIncrement());
+         Thread.sleep(10);
+      });
+
+      // Set the thread to run 10 repetitions
+      int repetitionsToRun = 10;
+      thread.setScheduled(repetitionsToRun);
+      assertState(thread, false, false, repetitionsToRun, false, 0);
+
+      // Start the thread. Should start running the repetitions
+      thread.start();
+      assertState(thread, true, true, repetitionsToRun, false, 0);
+
+      // Wait for all repetitions to complete
+      thread.blockUntilNoScheduledTasks();
+      assertState(thread, true, true, 0, false, repetitionsToRun);
+
+      // Kill the thread and wait for it to die
+      thread.kill();
+      assertState(thread, false, true, 0, false, repetitionsToRun);
+      assertDoesNotThrow(() -> thread.join(1000));
+      assertState(thread, false, false, 0, false, repetitionsToRun);
+
+      assertEquals(repetitionsToRun, repetitions.get());
+   }
+
+   @Test
+   public void testStartPauseStart()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, () ->
+      {
+         LogTools.info("Repetition {}", repetitions.getAndIncrement());
+         ThreadTools.park(0.01);
+      });
+
+      // Start repeating
+      thread.startRepeating();
+      assertState(thread, true, true, RepeatingTaskThread.REPEAT_INDEFINITELY, false, 0);
+
+      // Ensure a task starts
+      thread.blockUntilNextTaskExecution();
+      assertState(thread, true, true, RepeatingTaskThread.REPEAT_INDEFINITELY, true, 0);
+
+      // Stop repeating
+      thread.stopRepeating();
+      assertState(thread, true, true, 0, true, 0);
+
+      thread.blockUntilNextTaskCompletion();
+      assertState(thread, true, true, 0, false, 1);
+
+      // Start again
+      thread.startRepeating();
+      assertState(thread, true, true, RepeatingTaskThread.REPEAT_INDEFINITELY, false, 1);
+
+      ThreadTools.park(0.005);
+
+      // Kill the thread
+      thread.kill();
+      assertState(thread, false, true, 0, true, 1);
+      assertDoesNotThrow(() -> thread.join(1000));
+      assertState(thread, false, false, 0, false, 2);
+   }
+
+   @Test
+   public void testDoubleStart()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, () ->
+      {
+         LogTools.info("Repetition {}", repetitions.getAndIncrement());
+         ThreadTools.park(0.01);
+      });
+
+      thread.startRepeating();
+      assertState(thread, true, true, RepeatingTaskThread.REPEAT_INDEFINITELY, false, 0);
+
+      thread.startRepeating();
+      assertState(thread, true, true, RepeatingTaskThread.REPEAT_INDEFINITELY, false, 0);
+
+      ThreadTools.park(0.005);
+
+      thread.kill();
+      assertDoesNotThrow(() -> thread.join(1000));
+      assertState(thread, false, false, 0, false, 1);
+   }
+
+   @Test
+   public void testKillWithoutStart()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, () ->
+      {
+         LogTools.info("Repetition {}", repetitions.getAndIncrement());
+         Thread.sleep(10);
+      });
+
+      assertState(thread, false, false, 0, false, 0);
+
+      thread.blockingKill();
+      assertState(thread, false, false, 0, false, 0);
+
+      thread.blockingKill();
+      assertState(thread, false, false, 0, false, 0);
+   }
+
+   @Test
+   public void testInvalidStarts()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RunnableThatThrows runnable = () ->
+      {
+         LogTools.info("Repetition {}", repetitions.getAndIncrement());
+         Thread.sleep(10);
+      };
+
+      // Cannot call start when the thread is already running
+      RepeatingTaskThread thread1 = new RepeatingTaskThread(NAME, runnable);
+      thread1.startRepeating();
+      assertThrows(IllegalThreadStateException.class, thread1::start);
+      thread1.kill();
+
+      // Cannot restart after killing
+      RepeatingTaskThread thread2 = new RepeatingTaskThread(NAME, runnable);
+      thread2.startRepeating();
+      thread2.blockingKill();
+      assertThrows(IllegalThreadStateException.class, thread2::startRepeating);
+
+      // Can start after kill if the thread hasn't been started yet???
+//      RepeatingTaskThread thread0 = new RepeatingTaskThread(NAME, runnable);
+//      thread0.kill();
+//      assertThrows(IllegalThreadStateException.class, thread0::startRepeating);
+   }
+
+   @Test
+   public void testAddScheduledRepetitions()
+   {
+      AtomicInteger repetitions = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, repetitions::getAndIncrement);
+
+      int add = 20;
+      int subtract = -10;
+      int increment = 1;
+      int total = add + subtract + increment;
+
+      thread.addScheduled(add);
+      thread.addScheduled(subtract);
+      thread.start();
+
+      thread.addScheduled(increment);
+      thread.blockUntilNoScheduledTasks();
+      thread.kill();
+      assertEquals(total, repetitions.get());
+      assertEquals(total, thread.getCompleted());
+   }
+
+   @Test
+   public void testLoopFrequencyLimit()
+   {
+      FrequencyCalculator frequencyCalculator = new FrequencyCalculator();
+
+      double targetFrequency = 5.0;
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME, frequencyCalculator::ping).setFrequencyLimit(targetFrequency);
+
+      // Start repeating at the target frequency
+      thread.startRepeating();
+      ThreadTools.sleep(750);
+      assertEquals(targetFrequency, frequencyCalculator.getFrequency(), 0.2);
+
+      // Increase the target frequency
+      targetFrequency = 30.0;
+      thread.setFrequencyLimit(targetFrequency);
+      ThreadTools.sleep(750);
+      assertEquals(targetFrequency, frequencyCalculator.getFrequency(), 0.2);
+
+      // Un-limit the repetition frequency
+      thread.removeFrequencyLimit();
+      ThreadTools.sleep(750);
+      assertTrue(frequencyCalculator.getFrequency() > targetFrequency + 10.0); // Ensure thread is running at higher frequency than previous limit
+
+      thread.blockingKill();
+   }
+
+   @Test
+   public void testInterrupt()
+   {
+      AtomicInteger interruptCount = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME)
+      {
+         @Override
+         protected void runTask()
+         {
+            ThreadTools.park(0.01);
+
+            if (interrupted())
+               interruptCount.incrementAndGet();
+         }
+      };
+
+      LogTools.info("Test during free spin");
+      thread.startRepeating();
+      for (int i = 0; i < 25; ++i)
+      {
+         thread.blockUntilNextTaskExecution();
+         thread.interrupt();
+         thread.blockUntilNextTaskCompletion();
+         assertEquals(i + 1, interruptCount.get());
+      }
+
+      LogTools.info("Test during throttled looping");
+      interruptCount.set(0);
+      thread.setFrequencyLimit(200.0);
+      for (int i = 0; i < 25; ++i)
+      {
+         thread.blockUntilNextTaskExecution();
+         thread.interrupt();
+         thread.blockUntilNextTaskCompletion();
+         assertEquals(i + 1, interruptCount.get());
+      }
+
+      thread.stopRepeating();
+
+      LogTools.info("Test one by one");
+      interruptCount.set(0);
+      for (int i = 0; i < 25; ++i)
+      {
+         thread.addScheduled(1);
+         thread.blockUntilNextTaskExecution();
+         thread.interrupt();
+         thread.blockUntilNextTaskCompletion();
+         assertEquals(i + 1, interruptCount.get());
+      }
+
+      LogTools.info("Completed test");
+
+      thread.blockingKill();
+   }
+
+   @Test
+   public void testImmediateShutdown()
+   {
+      RunnableThatThrows wasteTime = () ->
+      {
+         try
+         {  // Sleep for half a second
+            Thread.sleep((long) Conversions.secondsToMilliseconds(0.5));
+         } catch (InterruptedException ignored) {}
+      };
+
+      LogTools.info("Test during free spin");
+      for (int millisToSleep = 0; millisToSleep < 500; millisToSleep += 100)
+      {
+         // Create a new thread
+         RepeatingTaskThread thread = new RepeatingTaskThread(NAME, wasteTime);
+
+         // Start free spin
+         thread.startRepeating();
+         ThreadTools.sleep(millisToSleep);
+
+         // Time the shutdown duration
+         long shutdownStart = System.nanoTime();
+         thread.kill();
+         thread.interrupt();
+         assertDoesNotThrow(() -> thread.join(500));
+         long shutdownComplete = System.nanoTime();
+
+         double shutdownDuration = Conversions.nanosecondsToSeconds(shutdownComplete - shutdownStart);
+         LogTools.info("Shutdown Duration: {}", shutdownDuration);
+         assertTrue(shutdownDuration < 0.01);
+      }
+
+      LogTools.info("Test during throttled looping");
+      for (int millisToSleep = 0; millisToSleep < 500; millisToSleep += 100)
+      {
+         // Create a new throttled thread
+         RepeatingTaskThread thread = new RepeatingTaskThread(NAME, wasteTime).setFrequencyLimit(1.0);
+
+         // Start throttled spin
+         thread.startRepeating();
+         ThreadTools.sleep(millisToSleep);
+
+         // Time the shutdown duration
+         long shutdownStart = System.nanoTime();
+         thread.kill();
+         thread.interrupt();
+         assertDoesNotThrow(() -> thread.join(500));
+         long shutdownComplete = System.nanoTime();
+
+         double shutdownDuration = Conversions.nanosecondsToSeconds(shutdownComplete - shutdownStart);
+         LogTools.info("Shutdown Duration: {}", shutdownDuration);
+         assertTrue(shutdownDuration < 0.01);
+      }
+
+      LogTools.info("Test during pause");
+      for (int millisToSleep = 0; millisToSleep < 500; millisToSleep += 100)
+      {
+         // Create a new throttled thread
+         RepeatingTaskThread thread = new RepeatingTaskThread(NAME, wasteTime);
+
+         // Start throttled spin
+         thread.start();
+         ThreadTools.sleep(millisToSleep);
+
+         // Time the shutdown duration
+         long shutdownStart = System.nanoTime();
+         thread.kill();
+         // No interrupted necessary in this case
+         assertDoesNotThrow(() -> thread.join(500));
+         long shutdownComplete = System.nanoTime();
+
+         double shutdownDuration = Conversions.nanosecondsToSeconds(shutdownComplete - shutdownStart);
+         LogTools.info("Shutdown Duration: {}", shutdownDuration);
+         assertTrue(shutdownDuration < 0.01);
+      }
+   }
+
+   @Test
+   public void testImmediateShutdownRace()
+   {
+      RunnableThatThrows wasteTime = () ->
+      {
+         try
+         {  // Sleep for 5 seconds
+            Thread.sleep(5000);
+         } catch (InterruptedException ignored) {}
+      };
+
+      for (int i = 0; i < 5000; ++i)
+      {
+         // Create a new thread
+         RepeatingTaskThread thread = new RepeatingTaskThread(NAME, wasteTime);
+
+         // Start free spin
+         thread.startRepeating();
+         LockSupport.parkNanos(5);
+
+         // Time the shutdown duration
+         long shutdownStart = System.nanoTime();
+         thread.kill();
+         thread.interrupt();
+         assertDoesNotThrow(() -> thread.join(500));
+         long shutdownComplete = System.nanoTime();
+
+         double shutdownDuration = Conversions.nanosecondsToSeconds(shutdownComplete - shutdownStart);
+         LogTools.info("Shutdown Duration: {}", shutdownDuration);
+         assertTrue(shutdownDuration < 0.01);
+      }
+   }
+
+   @Test
+   public void testOverride()
+   {
+      AtomicInteger loopCounter = new AtomicInteger(0);
+      RepeatingTaskThread thread = new RepeatingTaskThread(NAME)
+      {
+         @Override
+         protected void runTask()
+         {
+            loopCounter.set(loopCounter.get() + 1);
+         }
+      };
+
+      int targetLoops = 15;
+      thread.setScheduled(targetLoops);
+      thread.start();
+      thread.blockUntilNoScheduledTasks();
+      thread.blockingKill();
+      assertEquals(targetLoops, loopCounter.get());
+   }
+
+   private void assertState(RepeatingTaskThread thread, boolean running, boolean isAlive, long scheduled, boolean isExecuting, long completed)
+   {
+      assertEquals(running, thread.isRunning());
+      assertEquals(isAlive, thread.isAlive());
+      assertEquals(scheduled, thread.getScheduled());
+      assertEquals(isExecuting, thread.isExecuting());
+      assertEquals(completed, thread.getCompleted());
+   }
+}
